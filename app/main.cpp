9bcf7fafb9a427a400215ea8e9425bdcad10b3cd
Kirill Andriianov <andrianov.kirill2010@yandex.ru>
2023-10-29 22:12:27 +0100

    make demo able to run different frontends

--- app/main.cpp
+++ app/main.cpp
@@ -19,12 +19,12 @@
 // filename of rom to load and optional max cycles to emulate
 int main(int argc, char** argv)
 {
-    if (argc < 2) {
-        std::cerr << "usage: " << argv[0] << " <rom>" << std::endl;
+    if (argc < 3) {
+        std::cerr << "usage: " << argv[0] << " <sdl|ascii> <rom>" << std::endl;
         return 1;
     }
 
-    std::string_view rom_filename(argv[1]);
+    std::string_view rom_filename(argv[2]);
 
     // read rom from file
     chip8::bytes_owned rom = std::invoke([rom_filename]() {
@@ -43,27 +43,41 @@ int main(int argc, char** argv)
         .max_cycles = std::numeric_limits<uint64_t>::max(),
     };
 
-#if ENABLE_SDL
-    auto sdl_impl = std::make_unique<chip8::sdl::sdl_system_facade_t>();
+    auto init_with_sdl = [settings]() mutable {
+        auto sdl_impl = std::make_unique<chip8::sdl::sdl_system_facade_t>();
 
-    auto vm = std::make_unique<chip8::vm_t>(
-        std::move(settings),
-        *sdl_impl,
-        *sdl_impl,
-        *sdl_impl
-    );
-#else
-    auto keyboard_system = std::make_unique<chip8::keyboard_system_fake_t>();
-    auto timers_system = std::make_unique<chip8::timers_system_basic_t>();
-    auto video_system = std::make_unique<chip8::video_system_ascii_t>();
+        return std::make_unique<chip8::vm_t>(
+            std::move(settings),
+            *sdl_impl,
+            *sdl_impl,
+            *sdl_impl
+        );
+    };
 
-    auto vm = std::make_unique<chip8::vm_t>(
-        std::move(settings),
-        *keyboard_system,
-        *timers_system,
-        *video_system
+    auto init_with_ascii = [settings]() mutable {
+        auto keyboard_system = std::make_unique<chip8::keyboard_system_fake_t>();
+        auto timers_system = std::make_unique<chip8::timers_system_basic_t>();
+        auto video_system = std::make_unique<chip8::video_system_ascii_t>();
+
+        return std::make_unique<chip8::vm_t>(
+            std::move(settings),
+            *keyboard_system,
+            *timers_system,
+            *video_system
+        );
+    };
+
+    auto vm = std::invoke(
+        [argv, &init_with_sdl, &init_with_ascii]() {
+            if (std::string_view(argv[1]) == "sdl") {
+                return init_with_sdl();
+            } else if (std::string_view(argv[1]) == "ascii") {
+                return init_with_ascii();
+            } else {
+                throw std::runtime_error("unknown frontend type");
+            }
+        }
     );
-#endif
 
     vm->load_data(rom, chip8::ROM_OFFSET);
     vm->load_data(chip8::CHIP8_STANDARD_FONTSET_VIEW, 0);
