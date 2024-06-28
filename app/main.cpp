c0b7c8e7a311ebf1649f5791f384a0485e53754a
Kirill Andriyanov <andrianov.kirill2010@yandex.ru>
2024-06-29 00:02:13 +0200

    app/main.cpp small fixes: - main is defined by SDL for WATEVER reason - refactor arguments parsing just a bit

--- app/main.cpp
+++ app/main.cpp
@@ -12,34 +12,35 @@
 
 #include <impl_sdl/platform.h>
 
-
+#undef main
 int main(int argc, char** argv)
 {
-    if (argc < 3) {
+    if (argc < 4) {
         std::cerr << "usage: " << argv[0] << " <sdl|ascii> <ch8|sch|xoch> <rom>" << std::endl;
         return 1;
     }
 
-    std::string_view rom_filename(argv[3]);
+    auto renderer = std::string_view(argv[1]);
+    auto emulator_type = std::string_view(argv[2]);
+    auto rom_filename = std::string_view(argv[3]);
 
     // read rom from file
-    chip8::bytes_owned rom = std::invoke([rom_filename]() {
+    auto rom = std::invoke([rom_filename]() {
         std::ifstream file(rom_filename.data(), std::ios::in | std::ios::binary);
         if (!file.is_open()) {
             throw std::runtime_error("failed to open file");
         }
 
-        chip8::bytes_owned buffer(std::istreambuf_iterator<char>(file), {});
-        return buffer;
+        return chip8::bytes_owned(std::istreambuf_iterator<char>(file), {});
     });
 
     auto settings = chip8::vm_t::settings_t{
-        .emulator_type = std::invoke([argv]() {
-            if (std::string_view(argv[2]) == "ch8") {
+        .emulator_type = std::invoke([emulator_type]() {
+            if (emulator_type == "ch8") {
                 return chip8::vm_t::settings_t::CHIP_8;
-            } else if (std::string_view(argv[2]) == "sch") {
+            } else if (emulator_type == "sch") {
                 return chip8::vm_t::settings_t::SCHIP1_1;
-            } else if (std::string_view(argv[2]) == "xoch") {
+            } else if (emulator_type == "xoch") {
                 return chip8::vm_t::settings_t::XO_CHIP;
             } else {
                 std::cerr << "invalid emulator type" << std::endl;
@@ -88,9 +89,9 @@ int main(int argc, char** argv)
         vm->emulate_duration();
     };
 
-    if (std::string_view(argv[1]) == "sdl") {
+    if (renderer == "sdl") {
         run_with_sdl();
-    } else if (std::string_view(argv[1]) == "ascii") {
+    } else if (renderer == "ascii") {
         run_with_ascii();
     } else {
         std::cerr << "invalid frontend type" << std::endl;
